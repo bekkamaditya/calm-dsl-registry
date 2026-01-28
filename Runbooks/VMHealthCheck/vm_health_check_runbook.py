0b2f20f5b57adf1ecf5795b511253d81ff5fc3f2
bekkamaditya <bekkamvenkata.aditya@nutanix.com>
2026-01-28 20:55:47 +0530

    Fix compilation error

--- Runbooks/VMHealthCheck/vm_health_check_runbook.py
+++ Runbooks/VMHealthCheck/vm_health_check_runbook.py
@@ -9,21 +9,19 @@ from calm.dsl.runbooks import RunbookTask as Task
 from calm.dsl.runbooks import CalmEndpoint as Endpoint
 from calm.dsl.runbooks import ref
 
-WindowsVM = Endpoint.use_existing("WindowsEndpoint")
-
 
 @runbook
-def VMHealthCheckRunbook(endpoints=[WindowsVM]):
-    """Check disk stats and RAM/CPU utilisation on Windows VM."""
+def VMHealthCheckRunbook():
+    """Check disk stats and RAM/CPU utilisation on VMs."""
     # Task 1: Windows VM – disk usage
     Task.Exec.powershell(
         name="WindowsDiskStats",
         filename="scripts/windows_disk_check.ps1",
-        target=ref(endpoints[0]),
+        target=ref(Endpoint.use_existing("WindowsEndpoint")),
     )
     # Task 2: Windows VM – RAM and CPU utilisation
     Task.Exec.powershell(
         name="WindowsRAMCPUStats",
         filename="scripts/windows_ram_cpu_check.ps1",
-        target=ref(endpoints[0]),
+        target=ref(Endpoint.use_existing("WindowsEndpoint")),
     )
